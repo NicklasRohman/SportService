db9be55b3c0a916c38ee5ea5d37500defe72780e
NicklasRohman <NicklasRohman@gmail.com>
2017-11-12 09:11:08 +0100

    inga värden

--- src/rs/SportsingRSService.java
+++ src/rs/SportsingRSService.java
@@ -1,7 +1,7 @@
 package rs;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,25 +14,37 @@ import com.sportsing.api.Match;
 public class SportsingRSService {
 	private List<Match>matches;
 	
-	public SportsingRSService() {
-	}
-	
 	@GET
 	@Path("/list")
 	@Produces(MediaType.APPLICATION_XML)
 	public List<Match> getMatches(){
 		
+
+	System.out.println("GetMatches i Matches");
+			matches = new ArrayList<>();
+			matches.add(new Match("Fotboll","Iggy",3,1));
+			matches.add(new Match("Hocky","wolf",5,1));
+			matches.add(new Match("Innebandy","Nicklas",1,2));
+			matches.add(new Match("Golf","Nicklas",-7,1));
+			matches.add(new Match("Hapkido","Gustav",4,2));
 		return matches;
 	}
 	
 	@GET
 	@Path("/get/{matchname}")
 	@Produces(MediaType.APPLICATION_XML)
-	public Match getMatch(@PathParam("sport") String sport) throws IllegalAccessException{
+	public Match getMatch(@PathParam("sport") String sport){
 		
-		
-		throw new IllegalAccessException("No such match \" "+ sport +"\"");
+		for (Match match : matches) {
+			if (match.getContenderIterator().equals(sport)) {
+				return match;
+			}
+		}
+		return getMatch(sport);
 	}	
 }
+
+
+
 
 //http://localhost:8080/sportsing-webservice/rs/sports/list
